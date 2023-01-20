aa4c7077dec537cdbe6e48f4b9d8d4515fd87983
Douglas Campos <dougcampos1501@gmail.com>
2023-01-20 20:40:53 -0300

    route

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
-app.run()
+
+@app.route('/')
+def index():
+    return "Olá Mundo.. agora em Flask"
+
+@app.route('/get_service', methods=['GET'])
+def get_service():
+    return "Isto é um GET"
+
+@app.route('/post_service', methods=['POST'])
+def post_service():
+    return "Isto é um POST"
+
+@app.route('/html')
+def html():
+    return "<html><body><h1>Olá mundo em FLASK agora com H1</h1<</body></html>"
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return "Hello" + name
+
+app.run(debug=True)
